8dfc390764789ae4352a749f22e82d741f8fb8b8
Djordje Djordjic <djordje.djordjic@pixels2pixels.com>
2026-07-14 16:44:25 +0200

    Single product: add promo-video hero (hero-video-carousel) to top of series template per Figma

--- themes/wardjet/single-series.php
+++ themes/wardjet/single-series.php
@@ -4,7 +4,9 @@ get_header();
 $blocks = get_field('content');
 ?>
 <div id="product">
-    <?php 
+    <?php /* Promo Video hero (matches Figma single-product; same part as homepage/products). */ ?>
+    <?php get_template_part('template-parts/hero-video-carousel'); ?>
+    <?php
     if (is_array($blocks)):
         foreach($blocks as $block):
             get_template_part('template-parts/section', $block['acf_fc_layout'], ['block'=>$block]);
